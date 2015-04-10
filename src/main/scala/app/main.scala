fc9edd6fa3bbec4236695381a0058839ec5eab81
vipmax <mvp080808>
2015-04-10 13:37:19 +0400

    added filtering by time period added max,min,avg values

--- src/main/scala/app/main.scala
+++ src/main/scala/app/main.scala
@@ -13,9 +13,17 @@ object main {
      val sc = new SparkContext(conf)
 
      sc.cassandraTable("monitoring", "rawdata").
-       select("instanceid", "parameterid", "time", "timeperiod", "value").
-       groupBy(row => (row.get[Int]("instanceid"), row.get[Int]("parameterid"), row.get[DateTime]("time").getHourOfDay)).
-       map(r => (r._1, r._2.fold(0.0)((acc, cr) => acc.asInstanceOf[Double] + cr.asInstanceOf[CassandraRow].get[Double]("value")))).
+       select("instanceid", "parameterid","time", "timeperiod", "value").
+       where("timeperiod = ?", "1m").map(cr => {println("select " + cr); cr}).
+       groupBy(row => (row.get[Int]("instanceid"),row.get[Int]("parameterid"), row.get[DateTime]("time").getHourOfDay)).map(r=> { println("groupby "+r); r}).
+       map(r => {
+         val key = r._1  //instance id, parameter id,time hour
+         val sum = r._2.fold(0.0)((acc, cr) => acc.asInstanceOf[Double] + cr.asInstanceOf[CassandraRow].get[Double]("value"))
+         val max = r._2.fold(Double.MinValue)((max, cr) => Math.max(max.asInstanceOf[Double], cr.asInstanceOf[CassandraRow].get[Double]("value")))
+         val min = r._2.fold(Double.MaxValue)((min, cr) => Math.min(min.asInstanceOf[Double], cr.asInstanceOf[CassandraRow].get[Double]("value")))
+         val avg = (sum.asInstanceOf[Double])/(r._2.size)
+       (key, sum, max, min, avg)
+       }).
        foreach(r => println("end " + r))
    }
  }
